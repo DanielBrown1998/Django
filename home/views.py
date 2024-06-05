5a93dd4395cd7e968f5cca37d1cc050ea5c30777
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-06-05 20:18:09 -0300

    django lang

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Bem vindo ao Home!')
+    return render(request, 'home/home.html')
+
