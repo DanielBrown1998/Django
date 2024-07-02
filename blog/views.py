7596a41f798322960d2155089092a9c582c573e2
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-07-02 14:53:13 -0300

    como por em produçao

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from typing import Any
+from django.shortcuts import render, get_object_or_404
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from blog.data import posts
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     context = {
         "name": "BLOG",
         "title": "Blog",
@@ -12,12 +15,12 @@ def index(request):
     return render(request, 'blog/home.html',context=context)
 
 
-def post(request, pk: int):
-    found_post = None
+def post(request: HttpRequest, pk: int) -> HttpResponse: 
+    found_post: dict[str, Any] | None = None
     for item in posts:
         if item['id'] == pk:
             found_post = item
-    
+            break
 
     context = {
         "name": "BLOG",
